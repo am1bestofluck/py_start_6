08bc3b27fc6c8b504bb0f0f32465f486eb172d1f
[am_6733] <[anton6733@gmail.com]>
2023-01-18 16:55:26 +0200

    rdy

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 """
 progression - арифметическая прогрессия
 
@@ -7,7 +8,7 @@ pow-positive - возведение  a в степень б
 """
 # *lambda, filter, *map, *zip, *enumerate, *list comprehension
 
-
+import random
 from typing import List
 
 
@@ -21,6 +22,7 @@ def progression(start_: int, step: int, len_: int):
     values = list(map(lambda pos: start_ + (pos-1)*step, keys))
     out = list(zip(keys,values))
     return out
+
 
 def positions(lst: List[int | float] = [-5, 9, 0, 3, -1, -2, 1, 4, -2,
                                         10, 2, 0, -9, 8, 10, -9, 0, -5,
@@ -36,9 +38,15 @@ pow_positive = lambda a, b: 1 if b == 0 else a*pow_positive(a, b-1)
 
 
 def main():
-    print(progression(10,5,5))
+    print(progression(int(input("база прогрессии: ")),
+    int(input("шаг прогрессии: ")),
+    int(input("количество элементов: "))))
+
     assert positions() == [1, 9, 13, 14, 19]
-    assert pow_positive(2, 10) == 2**10
+    
+    for i in range(100):
+        picks = random.sample(range(1, 100),k=2)
+        assert pow_positive(picks[0], picks[1]) == picks[0]**picks[1]
 
 
 if __name__ == "__main__":
